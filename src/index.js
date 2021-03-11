f543f11b25d09b37f4251b137adbd58bb9c9fba1
Larry Hawelu <lhawelu@gmail.com>
2021-03-11 14:09:43 -0800

    commit tree a8dbe9ae48c1bad458523f88a1459fff40670e78

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded', function() {
+  function addImages() {
+    fetch('https://dog.ceo/api/breeds/image/random/4')
+    .then(resp => resp.json())
+    .then(dogList => {
+      dogList.message.forEach(dog => renderOneDog(dog))
+    })
+  }
+
+  function renderOneDog(dog) {
+    const dogDiv = document.createElement('div');
+    dogDiv.className = "dog-image";
+    dogDiv.innerHTML = `
+    <img src=${dog} alt='image of dog' width= '200'</img>
+    `
+    document.querySelector('#dog-image-container').appendChild(dogDiv)
+  }
+
+  addImages();
+
+  const breedSelect = document.querySelector('#breed-dropdown');
+  breedSelect.addEventListener('change', (e) => {
+    const letter = e.target.value;
+    document.querySelector('#dog-breeds').innerHTML = '';
+  
+
+    function createDogList() {
+      fetch('https://dog.ceo/api/breeds/list/all')
+      .then(resp => resp.json())
+      .then(dogList => Object.keys(dogList.message)
+        .filter(dog => dog[0] === letter)
+        .forEach(dog => createOneDog(dog)))
+      }
+
+    function createOneDog(dog) {
+      const dogLi = document.createElement('li');
+      dogLi.textContent = dog;
+      dogLi.addEventListener('click', () => {
+        dogLi.style.color === 'red' ? dogLi.style.color = 'black' : dogLi.style.color = 'red'
+      })
+      document.querySelector('#dog-breeds').appendChild(dogLi)
+    }
+    createDogList();
+  });
+});
